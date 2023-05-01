77b67a4ff99817570737e4a3a2ada8a61319118a
mohammadanas51 <132257373+mohammadanas51@users.noreply.github.com>
2023-05-01 13:56:00 +0530

    Add files via upload

---
+++ calculatorMini.py
@@ -0,0 +1,18 @@
+#Python program to build a mini calculator 
+first = input("Enter the first number : ")
+second = input("Enter the second number : ") 
+operator = input("Enter the operator(+,-,*,/,%) : ")
+first = int(first)
+second = int(second)
+if operator == "+" : 
+    print(first+second)
+elif operator == "-" : 
+    print(first-second)
+elif operator == "*" : 
+    print(first*second)
+elif operator == "/" : 
+    print(first/second)
+elif operator == "%" : 
+    print(first%second)
+else : 
+    print("Invalid Operation")
